5cefc94116feab829894078e93e230652a430d99
Dhederosade <febriansyahrevan7@gmail.com>
2020-11-25 23:14:47 +0700

    18090091

---
+++ Pengembangan_P10/app/src/main/java/com/revan/pengembangan_p10/NoteViewMode.kt
@@ -0,0 +1,30 @@
+package com.revan.pengembangan_p10
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import com.revan.pengembangan_p10.catatan.Note
+import com.revan.pengembangan_p10.catatan.NoteRepository
+
+
+class NoteViewMode(application: Application) : AndroidViewModel(application)
+{
+    private var repository: NoteRepository =
+        NoteRepository(application)
+    private var allNotes: LiveData<List<Note>> = repository.getAllNotes()
+    fun insert(note: Note) {
+        repository.insert(note)
+    }
+    fun update(note: Note) {
+        repository.update(note)
+    }
+    fun delete(note: Note) {
+        repository.delete(note)
+    }
+    fun deleteAllNotes() {
+        repository.deleteAllNotes()
+    }
+    fun getAllNotes(): LiveData<List<Note>> {
+        return allNotes
+    }
+}
